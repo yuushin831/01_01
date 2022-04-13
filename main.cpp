d9a543debdb2360eab704d417b25db328cb820d3
takei_yuushin0831 <k021g1262@g.neec.ac.jp>
2022-04-13 15:49:30 +0900

    バックバッファ

--- main.cpp
+++ main.cpp
@@ -162,6 +162,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 	// デスクリプタヒープの生成
 	device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
 
+	// バックバッファ
+	std::vector<ID3D12Resource*> backBuffers;
+	backBuffers.resize(swapChainDesc.BufferCount);
 
 // DirectX初期化処理 ここまで
 // ゲームループ
