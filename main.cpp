d89cd497817d9841639d2e4496f4b59c2dfb8f76
takei_yuushin0831 <k021g1262@g.neec.ac.jp>
2022-04-13 15:24:01 +0900

    デバイスの生成

--- main.cpp
+++ main.cpp
@@ -99,6 +99,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			break;
 		}
 	}
+	
+	// 対応レベルの配列
+	D3D_FEATURE_LEVEL levels[] = 
+	{
+	D3D_FEATURE_LEVEL_12_1,
+	D3D_FEATURE_LEVEL_12_0,
+	D3D_FEATURE_LEVEL_11_1,
+	D3D_FEATURE_LEVEL_11_0,
+	};
+
+	D3D_FEATURE_LEVEL featureLevel;
+	for (size_t i = 0; i < _countof(levels); i++) {
+		// 採用したアダプターでデバイスを生成
+		result = D3D12CreateDevice(tmpAdapter, levels[i],
+			IID_PPV_ARGS(&device));
+		if (result == S_OK) {
+			// デバイスを生成できた時点でループを抜ける
+			featureLevel = levels[i];
+			break;
+		}
+	}
+
 
 // DirectX初期化処理 ここまで
 // ゲームループ
